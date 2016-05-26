60f4c647ce9712da1fd8ce1e86cef314e4e74938
Shawn5413 <tsubasa5413@yahoo.co.jp>
2016-05-26 20:06:21 +0900

    make new app which is the game to touch number

---
+++ app/src/main/java/com/shawn_wind/touchnumber/StartActivity.java
@@ -0,0 +1,35 @@
+package com.shawn_wind.touchnumber;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+
+public class StartActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        Button startButton = (Button) findViewById(R.id.button);
+        startButton.setOnClickListener(this);
+
+    }
+    @Override
+    public  void onClick(View start){
+        Intent startIntent = new Intent(StartActivity.this, MainActivity.class);
+        startActivity(startIntent);
+
+        GridLayout GL = new GridLayout(this);
+
+        GL.setColumnCount(4);   //Buttonの数
+        setContentView(GL);
+
+        Button topButton = new Button (this);
+        topButton.setText("1");
+        GL.addView(topButton);
+    }
+
+}
